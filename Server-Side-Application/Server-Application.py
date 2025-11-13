5566999f725ef7531b93c955d9e8b185aa6a37bd
Thomas Mendez <tmendez7524@floridapoly.edu>
2025-11-13 21:26:34 +0000

    Implemented downloading files from user
    downloadFiles function added, sendFiles function modified, a lot of handle_client modified as well

--- Server-Side-Application/Server-Application.py
+++ Server-Side-Application/Server-Application.py
@@ -14,15 +14,16 @@ SERVER_PATH = "server"
 def files_Set(directory):
     """Take a directory and return a set of all files within the directory"""
     filesSet = set()
-
     for entry in directory.iterdir():
         if entry.is_file():
             filesSet.add(entry.name)
-    
     return filesSet
-        
+
 def sendFiles(conn, fileName: Path()):
     """Send file to client"""
+    print(fileName)
+    name = fileName.name
+    conn.send(name.encode(FORMAT))
     with open(fileName, 'rb') as file:
         while True:
             print("Sending...")
@@ -30,9 +31,52 @@ def sendFiles(conn, fileName: Path()):
             if not file_content:
                 break
             conn.sendall(file_content)
-
+            conn.recv(SIZE).decode(FORMAT)
     conn.send(b"END")
+    print("File sent")
 
+def downloadFile(client):
+    """Recieve file from client and save"""
+    #Set the files name so that it will be saved in downloadable-storage
+    folderPath = Path("downloadable-storage")
+    savedName = client.recv(SIZE).decode(FORMAT)
+    savedNamePath = Path(savedName)
+    savedNamePath = Path(folderPath / savedNamePath.name)
+    print(savedNamePath)
+    
+    try: #Check if the file already exists in the server
+        #If not, make the new file
+        with open(f"{savedNamePath}", 'xb') as file:
+            client.send(b"OK")
+            while True:
+                print("Receiving...")
+                data = client.recv(SIZE)
+                print(data)
+                if data == b"END":
+                    break
+                file.write(data)
+                #Like this doesn't need to say anything lol just exists to make sure b"END" is sent as its own lol
+                client.send("NEXT".encode(FORMAT))       
+        print("completed task")
+
+    except FileExistsError as e: #If yes, check if the user wants to replace the old one with the new one
+        client.send("Error".encode(FORMAT))
+        print("Exception occured")
+        response = client.recv(SIZE)
+        print(response)
+        if response == b"OK": #If the user wants to replace the file, replace it
+            with open(f"{savedNamePath}", 'wb') as file:
+                while True:
+                    print("Receiving...")
+                    data = client.recv(SIZE)
+                    print(data)
+                    if data == b"END":
+                        break
+                    file.write(data)
+                    client.send("NEXT".encode(FORMAT))
+                    
+            print("downloaded files")
+    pass
 
 ### to handle the clients
 def handle_client (conn,addr):
@@ -42,6 +86,7 @@ def handle_client (conn,addr):
         data =  conn.recv(SIZE).decode(FORMAT)
         data = data.split("@")
         cmd = data[0]
+        print(cmd)
         p = Path("SocketServerRel.py")
         q = p.absolute()
        
@@ -58,7 +103,7 @@ def handle_client (conn,addr):
             
         
         # Print list of files in the downloadable storage folder
-        elif cmd == "FILES":
+        elif cmd == "Dir":
             folder = Path(q.parent / "downloadable-storage")
             print(f"{send_data}")
             #send_data += "LOGOUT from the server.\n"
@@ -66,13 +111,29 @@ def handle_client (conn,addr):
             send_data += f"{setofFIles}\n"
             conn.send(send_data.encode(FORMAT))
 
-        # TODO Allow for fileName to be inputed by user
-        elif cmd == "DOWNLOAD":
-            fileName = Path(q.parent / "downloadable-storage/25mb-text-file.txt")
-            sendFiles(conn, fileName)
+        elif "Upload " in cmd:
+            downloadFile(conn)
             send_data += "Message from the server.\n"
             conn.send(send_data.encode(FORMAT))
             print("Completed task")
+
+            
+            
+
+        elif "Download " in cmd:
+            fileName = cmd.replace("Download ",'',1)
+            print("Recieved")
+            try:
+                fileNamePath = Path(q.parent / f"downloadable-storage/{fileName}")
+            except FileNotFoundError as e:
+                print("Error: File Not Found")
+                send_data += "Error: File Not Found"
+                conn.send(send_data.encode(FORMAT))
+            else:
+                sendFiles(conn, fileNamePath)
+                send_data += "Message from the server.\n"
+                conn.send(send_data.encode(FORMAT))
+                print("Completed task")
 
     print(f"{addr} disconnected")
     conn.shutdown(socket.SHUT_WR)
