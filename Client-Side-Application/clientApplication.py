90fcebbc238927c7085a9826de94ebd2c4032b26
Abbey Reith <151561608+abbeyreith@users.noreply.github.com>
2025-11-13 17:15:05 -0500

    add authorization and hashing

--- Client-Side-Application/clientApplication.py
+++ Client-Side-Application/clientApplication.py
@@ -1,6 +1,7 @@
 import os
 import socket
 import json
+import hashlib #added for hashing
 
 # IP = "192.168.1.101" #"localhost"
 IP = "localhost"
@@ -26,12 +27,39 @@ def downloadFile(client, savedName):
     print("completed task")
     pass
 
+def authenticate(client):
+    """send username + hashed password to server then wait for approval."""
+    username = input("Username: ")
+    password = input("Password: ")
+
+    #hash password (no plaintext over network)
+    salt = "CNT3004"
+    hashed_pw = hashlib.sha256((password + salt).encode()).hexdigest()
+
+    #send authentication package
+    auth_msg = f"AUTH@{username}@{hashed_pw}"
+    client.send(auth_msg.encode(FORMAT))
+
+    #wait for server response
+    response = client.recv(SIZE).decode(FORMAT)
+
+    if response == "AUTH_OK":
+        print("Authentication successful!\n")
+        return True
+    else:
+        print("Authentication failed. Disconnecting.")
+        return False
+
 def main():
     client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     client.connect(ADDR)
     client.send('hello world CNT 3004 \n'.encode())
-    
-    
+
+    #authentication step
+    if not authenticate(client):
+        client.close()
+        return
+
     while True: ### multiple communications
         data = client.recv(SIZE).decode(FORMAT)
         cmd, msg = data.split("@")
@@ -53,16 +81,10 @@ def main():
             break
         elif cmd == "FILES":
             client.send(cmd.encode(FORMAT))
-        
         # TODO Have user input which file they want to download (will probably be done with UI stuff idk I just needed this to test my code)
         elif cmd == "DOWNLOAD":
             client.send(cmd.encode(FORMAT))
             downloadFile(client, "test.txt")
-
-
-            
-            
-
         
     print("Disconnected from the server.")
     client.shutdown(socket.SHUT_WR)
