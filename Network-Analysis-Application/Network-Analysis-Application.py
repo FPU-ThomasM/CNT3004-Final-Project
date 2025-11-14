14665bd169f4dc58232479e2e8de4c305ebd333b
Samuel Gates <134094466+Samuel-Gates@users.noreply.github.com>
2025-11-14 15:49:53 -0500

    Add main function as a placeholder

--- Network-Analysis-Application/Network-Analysis-Application.py
+++ Network-Analysis-Application/Network-Analysis-Application.py
@@ -6,3 +6,6 @@ import json
 import hashlib #these imports are based off the other modules
 
 #this is a little easter egg, hi y'all!
+
+def main():
+  return 0 #placeholder code for me a place to start
