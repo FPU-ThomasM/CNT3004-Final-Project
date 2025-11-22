634eb639926225738cb50a431428d1cfb46359b1
Samuel Gates <134094466+Samuel-Gates@users.noreply.github.com>
2025-11-22 03:39:24 -0500

    Enhance Analysis class with comments for timing functions
    Added comments for future functionality regarding start and stop time calculations.

--- Network-Analysis-Application/Network-Analysis-Application.py
+++ Network-Analysis-Application/Network-Analysis-Application.py
@@ -11,8 +11,17 @@ class Analysis:
     self.address = address
     
   #this is a little easter egg, hi y'all!
+
+  #Need a start time and a stop time function (replace record)
+  #calculate transmission rate when stop time is called
+  #stop time - start time = total time
+  #time will take in from computer's current time 
+  
+  
   def record_stats():
     return 0 #placeholder code for me a place to start
   
   def save_stats():
     return 0 #placeholder 2
+    #this function needs to be both for the server AND client
+
