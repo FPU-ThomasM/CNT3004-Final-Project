c7a3fba34973589ff10cb39dd0d302747ecda7d3
Samuel Gates <134094466+Samuel-Gates@users.noreply.github.com>
2025-11-24 13:11:50 -0500

    Implement transmission rate calculation and stats saving (and more)
    Added functionality to calculate and save transmission statistics.

--- Network-Analysis-Application/Network-Analysis-Application.py
+++ Network-Analysis-Application/Network-Analysis-Application.py
@@ -25,18 +25,40 @@ class Analysis:
     self.start_time_value = time.perf_counter()
 
     if file_path is not None and os.path.exists(file_path):
-        self.file_size = os.path.getsize(file_path)
+      self.file_size = os.path.getsize(file_path)
     return self.start_time_value
 
   def stop_time(): #this replaces save stats, for both server and client
     self.end_time_value = time.perf_counter()
 
     if self.start_time_value is None:
-        raise ValueError("start_time() must be called before stop_time().")
+      raise ValueError("start_time() must be called before stop_time().")
 
     total_time = self.end_time_value - self.start_time_value
 
     transmission_rate = self.file_size / total_time
+
+    self.stats = {
+      "role": self.role,
+      "ip_address": self.address,
+      "file_size_bytes": self.file_size,
+      "total_time_seconds": round(total_time, 4),
+      "transmission_rate_bps": round(transmission_rate, 2) if transmission_rate else None,
+      "transmission_rate_mbps": round((transmission_rate / (1024 * 1024)), 4) if transmission_rate else None
+    }
+
+    return self.stats
+
+  def save_stats(self, filename="analysis_results.json")
+    results_path = Path("results")
+    results_path.mkdir(exist_ok=True)
+
+    json_path = results_path / filename
+
+    with open(json_path, "w") as f:
+      json.dump(self.stats, f, indent=4)
+
+    return json_path
 
 
 
