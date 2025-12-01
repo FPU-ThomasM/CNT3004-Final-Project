ff13867de6cc3186b86afb7d0ab0c91b34684908
Samuel Gates <134094466+Samuel-Gates@users.noreply.github.com>
2025-12-01 16:22:24 -0500

    Comments added
    Removed unnecessary comments and added other comments

--- Network-Analysis-Application/Network-Analysis-Application.py
+++ Network-Analysis-Application/Network-Analysis-Application.py
@@ -6,9 +6,7 @@ import json
 import hashlib #these imports are based off the other modules
 import time
 
-#Hey all y'all, imma add the comments soon
 SIZE = 65536 ## byte .. buffer size
-
 
 class Analysis:
   def __init__(self, role, address): #you need to define the class, figure out the role, and get the IP address for each part
@@ -33,17 +31,17 @@ class Analysis:
       self.file_size = os.path.getsize(file_path)
     return self.start_time_value
 
-  def stop_time(self): #this replaces save stats, for both server and client
+  def stop_time(self): #for both server and client
     self.end_time_value = time.perf_counter()
 
-    if self.start_time_value is None:
+    if self.start_time_value is None: #maybe I remove this, don't want to crash the program with this module. worse that can happen is that things don't save
       raise ValueError("start_time() must be called before stop_time().")
 
     total_time = self.end_time_value - self.start_time_value
 
     transmission_rate = self.file_size / total_time
 
-    self.stats = {
+    self.stats = { #this is a python dictionary
       "role": self.role,
       "ip_address": self.address,
       "file_size_bytes": self.file_size,
@@ -51,9 +49,11 @@ class Analysis:
       "transmission_rate_bps": round(transmission_rate, 2) if transmission_rate else None,
       "transmission_rate_mbps": round((transmission_rate / (SIZE * SIZE)), 4) if transmission_rate else None
     }
-
+    
     return self.stats
 
+
+  #this module saves the stats to computer running it
   def save_stats(self, filename="analysis_results.json"):
     results_path = Path("results")
     results_path.mkdir(exist_ok=True)
@@ -64,6 +64,7 @@ class Analysis:
       json.dump(self.stats, f, indent=4)
 
     return json_path
+
 
 
 
